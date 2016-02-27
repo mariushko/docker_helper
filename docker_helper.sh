92a4cb12688a99eff2561874d7b348a059d161c6
Mariusz Bartusiak <mariushko@gmail.com>
2016-02-27 12:32:29 +0100

    refactoring

--- docker_helper.sh
+++ docker_helper.sh
@@ -22,63 +22,63 @@
 #
 ########################################################################
 
-_normalize_and_verify_image_name_(){
+dh__normalize_and_verify_image_name_(){
   local NAME=${1}
   local REPO TAG
   [[ -n ${NAME} ]] || return
   REPO=$(echo ${NAME} | cut -d: -f1)
   TAG=$(echo ${NAME} | cut -d: -f2)
   [[ ${NAME} == ${REPO} ]] && NAME=${NAME}:latest
-  list_all_images | grep -q "^${NAME}$" || return
+  dh_list_all_images | grep -q "^${NAME}$" || return
   echo ${NAME}
 }
 
-list_all_containers_by_name(){
+dh_list_all_containers_by_name(){
   local LIST=$(docker ps --all --quiet --no-trunc)
   [[ -n ${LIST} ]] || return
   docker inspect --format='{{.Name}}' ${LIST} | sed 's#^/##'
 }
 
-list_running_containers_by_name(){
+dh_list_running_containers_by_name(){
   local LIST=$(docker ps --quiet --no-trunc)
   [[ -n ${LIST} ]] || return
   docker inspect --format='{{.Name}}' ${LIST} | sed 's#^/##'
 }
 
-list_non_running_containers_by_name(){
+dh_list_non_running_containers_by_name(){
   local A B
   while read A
   do
     while read B
     do
       [[ ${A} == ${B} ]] && continue 2
-    done < <(list_running_containers_by_name)
+    done < <(dh_list_running_containers_by_name)
     echo ${A}
-  done < <(list_all_containers_by_name)
+  done < <(dh_list_all_containers_by_name)
 }
 
-list_running_containers_created_from_image(){
+dh_list_running_containers_created_from_image(){
   local NAME=${1}
   local ID
   [[ -n ${NAME} ]] || return
-  NAME=$(_normalize_and_verify_image_name_ ${NAME})
+  NAME=$(dh__normalize_and_verify_image_name_ ${NAME})
   ID=$(docker images --format='{{.ID}}' ${NAME})
   docker inspect --format='{{.Name}}' $(docker ps --quiet --no-trunc --filter ancestor=${ID}) | sed 's#^/##'
 }
 
-list_all_images(){
+dh_list_all_images(){
   docker images --format='{{.Repository}}:{{.Tag}}'
 }
 
-remove_non_running_containers(){
-  docker rm $(list_non_running_containers_by_name)
+dh_remove_non_running_containers(){
+  docker rm $(dh_list_non_running_containers_by_name)
 }
 
-kill_running_containers(){
-  docker kill $(list_running_containers_by_name)
+dh_kill_running_containers(){
+  docker kill $(dh_list_running_containers_by_name)
 }
 
-kill_and_remove_all_containers(){
-  kill_running_containers
-  remove_non_running_containers
+df_kill_and_remove_all_containers(){
+  dh_kill_running_containers
+  dh_remove_non_running_containers
 }
